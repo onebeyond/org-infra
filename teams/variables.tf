fa7d53feefe3143b4c71d5363caed304d7c545cc
Paula-Encinar <pencinarsanz@gmail.com>
2023-09-20 10:27:24 +0200

    Removing some variables

--- teams/variables.tf
+++ teams/variables.tf
@@ -1,8 +1,3 @@
-# variable "github_team_name" {
-#   description = "The name of the GitHub team"
-#   type        = string
-# }
-
 variable "github_users" {
   description = "A list of GitHub users to add to the team"
   type = list(object({
