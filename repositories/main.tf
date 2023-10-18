bfc043f0e073307fad02af45cce09c12abf3e905
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2023-10-18 15:12:00 +0200

    fix: add permissions to new repos

--- repositories/main.tf
+++ repositories/main.tf
@@ -10,7 +10,6 @@ module "repository-warthog_load_testing" {
     team_id    = "team-triage"
     permission = "triage"
   }]
-
 }
 
 module "repository-xk6-mongo" {
@@ -24,15 +23,27 @@ module "repository-xk6-mongo" {
 module "repository-actions" {
   source = "./templates"
   github_repository = {
-    name       = "actions"
-    visibility = "public"
+    name        = "actions"
+    description = "A collection of GitHub actions to use in our organization"
+    visibility  = "public"
   }
+
+  github_teams_repository = [{
+    team_id    = "team-actions"
+    permission = "maintain"
+  }]
 }
 
 module "repository-morning-slackbot" {
   source = "./templates"
   github_repository = {
-    name       = "morning-slackbot"
-    visibility = "public"
+    name        = "morning-slackbot"
+    description = "A Slackbot that sends a message to a channel every morning"
+    visibility  = "public"
   }
+
+  github_teams_repository = [{
+    team_id    = "team-one-beyond-employees"
+    permission = "maintain"
+  }]
 }
